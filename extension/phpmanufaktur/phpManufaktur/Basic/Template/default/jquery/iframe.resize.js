eee581d145627b482bef06f3e49b414ba18785f7
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-04-05 16:09:13 +0200

    Removed the BASIC extension from from the kitFramework core

--- extension/phpmanufaktur/phpManufaktur/Basic/Template/default/jquery/iframe.resize.js
+++
@@ -1,7 +0,0 @@
-if (typeof 'jQuery' != 'undefined') {
-  $(document).ready(function() {
-	  // setting the iFrame height dynamically by the height of the inbounded document
-	  var if_height = $('#kf_body').height() + 30 + "px";
-	  parent.document.getElementById("kf_iframe").style.height = if_height;
-  });
-}
